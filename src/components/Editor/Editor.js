e97389186cac3f93b266d91d1eee658259f1fcb5
VK <vitaliy.krynytskyy@gmail.com>
2019-08-02 13:48:57 +0100

    Both containers now look identical

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -6,7 +6,7 @@ class Editor extends React.Component {
     render() {
         return (
             <div className="editor">
-                <textarea className="textArea" placeholder="Edit me" onChange={(e) => { this.props.setMarkdown(e.target.value) }} />
+                <textarea placeholder="Edit me" onChange={(e) => { this.props.setMarkdown(e.target.value) }} />
             </div>
         )
     }
